434f60ef80b1ba01f85ddce8dcf3f2a6aba0dbe6
Ian Harwell <sirharwell@gmail.com>
2018-03-14 10:07:16 -0600

    init

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store';
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDOM.render(
+<provider store= {store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</provider>
+ document.getElementById('root'));
+registerServiceWorker();
---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import apps from './apps';
+
+const rootReducer = combineReducers({
+  apps,
+)}
